9d473ff316fde272890bc416db7d60d7470557cd
Beatriz Andrade <beatriz.andrade@edu.senai.br>
2026-07-31 11:03:29 -0300

    Criei a pastar de java e o arquivo de módulo 1 para início da matéria.

---
+++ Módulo_1/apresentacao.js
@@ -0,0 +1,9 @@
+console.log("Nome: Beatriz");
+console.log("Idade: 16 anos");
+console.log("Cidade: Santa Rita do Passa Quatro");
+console.log("Filme/Série favorito: Teen Wolf e Ratatouille");
+console.log("Time de futebol: Corinthians");
+console.log("Comida favorita: Macarrão");
+console.log("Hobby: Leitura");
+console.log("Música favorita: Fine Line");
+console.log("Animal de estimação: Hamster e Cachorro")
---
+++ Módulo_1/variavel.js
@@ -0,0 +1,13 @@
+//Variável é um espaço na memória para guardar um valor
+
+//Utilizamos duas formas para declarar variáveis em js
+
+//1 - const 
+//Atribuindo 
+const nome = "Beatriz";
+
+//Exibindo a atribuição
+console.log(nome);
+
+let nomeCachorro = "Bob";
+console.log(nomeCachorro);
